6ea5e5d9d4d81cd03f83172eef7b9c6183293443
Farruco Sanjurjo <farruco.sanjurjo@trabesoluciones.net>
2012-04-10 17:50:43 +0200

    Some refucktoring

--- test/spec/coello_queue_spec.erl
+++ test/spec/coello_queue_spec.erl
@@ -4,11 +4,18 @@
 -include_lib("amqp_client/include/amqp_client.hrl").
 
 spec() ->
+  before_all(fun() ->
+        meck:new([amqp_connection, amqp_channel]),
+        meck:expect(amqp_connection, start, 1, connection),
+        meck:expect(amqp_connection, open_channel, 1, {ok, channel})
+    end),
+
+  after_all(fun() ->
+        meck:unload([amqp_connection, amqp_channel])
+    end),
+
   describe("declare/1", fun() ->
         it("should create a exclusive, server named queue", fun()->
-              meck:new([amqp_connection, amqp_channel]),
-              meck:expect(amqp_connection, start, 1, connection),
-              meck:expect(amqp_connection, open_channel, 1, {ok, channel}),
               meck:expect(amqp_channel, call, 2, #'queue.declare_ok'{ queue = <<"abc">>}),
 
               Params = #'queue.declare'{exclusive = true},
@@ -17,16 +24,11 @@ spec() ->
               {ok, QueueName} = coello_queue:declare(Channel),
 
               assert_that(meck:called(amqp_channel, call, [Channel, Params]), is(true)),
-              assert_that(QueueName, is(<<"abc">>)),
-
-              meck:unload([amqp_connection, amqp_channel])
+              assert_that(QueueName, is(<<"abc">>))
         end)
   end),
 describe("bind/4", fun() ->
       it("should bind the queue with the passed in routing key", fun()->
-            meck:new([amqp_connection, amqp_channel]),
-            meck:expect(amqp_connection, start, 1, connection),
-            meck:expect(amqp_connection, open_channel, 1, {ok, channel}),
             meck:sequence(amqp_channel, call, 2,
               [#'queue.declare_ok'{}, #'queue.bind_ok'{}]),
 
@@ -39,8 +41,6 @@ describe("bind/4", fun() ->
             Params     = #'queue.bind'{queue = QueueName, exchange = Exchange, routing_key = RoutingKey },
             coello_queue:bind(Channel, QueueName, RoutingKey, Exchange),
 
-            assert_that(meck:called(amqp_channel, call, [Channel, Params]), is(true)),
-
-            meck:unload([amqp_connection, amqp_channel])
+            assert_that(meck:called(amqp_channel, call, [Channel, Params]), is(true))
         end)
   end).
