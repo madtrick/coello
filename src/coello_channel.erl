048e07d4577c34e72f691adec98630d9f31c1f79
Farruco Sanjurjo <farruco.sanjurjo@trabesoluciones.net>
2012-04-11 19:03:59 +0200

    coello_channel:close

--- src/coello_channel.erl
+++ src/coello_channel.erl
@@ -1,8 +1,12 @@
 -module(coello_channel).
 
--export([open/1]).
+-export([open/1, close/1]).
 
 -spec open(Connection::pid()) -> pid().
 open(Connection) ->
   {ok, Channel} = amqp_connection:open_channel(Connection),
   Channel.
+
+-spec close(Channel::pid()) -> ok | closing.
+close(Channel) ->
+  amqp_channel:close(Channel).
--- test/spec/coello_channel_spec.erl
+++ test/spec/coello_channel_spec.erl
@@ -4,17 +4,30 @@
 -include_lib("amqp_client/include/amqp_client.hrl").
 
 spec() ->
+  before_all(fun() ->
+        meck:new([amqp_connection, amqp_channel]),
+        meck:expect(amqp_connection, start, 1, pid),
+        meck:expect(amqp_connection, open_channel, 1, {ok, channel})
+    end),
+  after_all(fun() ->
+        meck:unload([amqp_connection, amqp_channel])
+    end),
   describe("open", fun() ->
         it("should open a channel on the passed connection", fun() ->
-              meck:new(amqp_connection),
-              meck:expect(amqp_connection, start, 1, pid),
-              meck:expect(amqp_connection, open_channel, 1, {ok, channel}),
-
               Connection = coello_connection:start(),
               coello_channel:open(Connection),
 
-              assert_that(meck:called(amqp_connection, open_channel, [Connection]), is(true)),
-              meck:unload(amqp_connection)
+              assert_that(meck:called(amqp_connection, open_channel, [Connection]), is(true))
           end)
-    end).
+    end),
+  describe("close", fun() ->
+        it("should close a channel on the passed connection", fun() ->
+              meck:expect(amqp_channel, close, 1, ok),
 
+              Connection = coello_connection:start(),
+              Channel = coello_channel:open(Connection),
+              coello_channel:close(Channel),
+
+              assert_that(meck:called(amqp_channel, close, [Channel]), is(true))
+          end)
+  end).
