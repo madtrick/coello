9290634a01856a1adf72103657ee53f7fc245075
Farruco Sanjurjo <farruco.sanjurjo@trabesoluciones.net>
2012-04-11 19:06:21 +0200

    Refactorize meck

--- test/spec/coello_connection_spec.erl
+++ test/spec/coello_connection_spec.erl
@@ -5,26 +5,27 @@
 -include_lib("amqp_client/include/amqp_client.hrl").
 
 spec() ->
+  before_all(fun()->
+        meck:new(amqp_connection),
+        meck:expect(amqp_connection, start, 1, ok)
+    end),
+  after_all(fun() ->
+        meck:unload(amqp_connection)
+    end),
   describe("start/0", fun() ->
         it("should open a connection using the default parameters", fun() ->
-              meck:new(amqp_connection),
-              meck:expect(amqp_connection, start, 1, ok),
               AmqpParams = #amqp_params_network{},
               coello_connection:start(),
 
-              assert_that(meck:called(amqp_connection, start, [AmqpParams]), is(true)),
-              meck:unload(amqp_connection)
+              assert_that(meck:called(amqp_connection, start, [AmqpParams]), is(true))
           end)
     end),
   describe("close/1", fun() ->
         it("should close the connection", fun()->
-              meck:new(amqp_connection),
-              meck:expect(amqp_connection, start, 1, ok),
               meck:expect(amqp_connection, close, 1, ok),
               Connection = coello_connection:start(),
               coello_connection:close(Connection),
 
-              assert_that(meck:called(amqp_connection, close, [Connection]), is(true)),
-              meck:unload(amqp_connection)
+              assert_that(meck:called(amqp_connection, close, [Connection]), is(true))
         end)
   end).
